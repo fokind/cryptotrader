3c3f053ef533d730f93d2d8b738d008f5dadcc1b
Dmitry Fokin <fokind1@yandex.ru>
2019-04-07 23:47:02 +0300

    диалог создания бэктеста

--- static/webapp/controller/Strategy.controller.js
+++ static/webapp/controller/Strategy.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/Fragment"
+], function (Controller, UIComponent, Fragment) {
 	"use strict";
 
 	return Controller.extend("fokin.crypto.controller.Strategy", {
@@ -13,23 +14,58 @@ sap.ui.define([
 			var mArguments = oEvent.getParameter("arguments");
 			var sId = mArguments.id;
 			this.getView().bindElement("data>/Strategies(\'" + sId + "\')");
-		},
+			// добавить draft
 
-		onAddBacktestPress: function(oEvent) {
-			var oView = this.getView();
-			oView.byId("backtests").getBinding("items").create({
+			this.getView().getModel("view").setProperty("/Draft", {
 				symbolFrom: "BTC",
 				symbolTo: "XRP",
 				period: "M1",
 				length: 1000,
+			});
+		},
+
+		onAddBacktestPress: function(oEvent) {
+			var oView = this.getView();
+
+			// create dialog lazily
+			if (!this.byId("createBacktestDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "fokin.crypto.fragment.CreateBacktestDialog",
+					controller: this
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+
+					// подписаться на нажатие кнопки
+					oDialog.open();
+				});
+			} else {
+				this.byId("createBacktestDialog").open();
+			}			
+		},
+
+		onCreateBacktestDialogOk: function() {
+			this.byId("createBacktestDialog").close();
+			// добавить индикатор загрузки, пока данные и модель обновляются
+
+			// дождаться сохранения
+			var oView = this.getView();
+			var oDraft = oView.getModel("view").getProperty("/Draft");
+			
+			oView.byId("backtests").getBinding("items").create({
+				symbolFrom: oDraft.symbolFrom,
+				symbolTo: oDraft.symbolTo,
+				period: oDraft.period,
+				length: oDraft.length,
 			}).created().then(function() {
 				oView.getBindingContext("data").refresh();
 			});
+		},
 
-			// открыть диалог с опциями
-			// добавить новый элемент
-			// дождаться выполнения
-			// закрыть диалог
+		onCreateBacktestDialogCancel: function() {
+			this.byId("createBacktestDialog").close();
 		},
 
 		onBacktestPress: function(oEvent) {
