b37797dfab43c6c8ddee4c70b30daa5aae39ea56
Dmitry Fokin <fokind1@yandex.ru>
2019-04-08 19:50:43 +0300

    работает с cryptocompare

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var { CryptoServer } = require("./server/lib/ts/server");
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(express.static('static/webapp'));
--- server/market.js
+++ server/market.js
@@ -1,19 +1,28 @@
 const request = require('request');
+const moment = require('moment');
+
+const API_KEY = process.env.API_KEY;
 
 function getCandles({ fsym, tsym, period, limit }, callback) { // TODO этот метод вынести в коннектор к бирже или др. поставщику статистики
+  const url = period === 'M1' ? 'histominute' : (period === 'H1' ? 'histohour' : 'histoday');
+
   request.get({
-    baseUrl: 'https://api.hitbtc.com/api/2/',
-    url: `public/candles/${tsym}${fsym}`,
+    baseUrl: 'https://min-api.cryptocompare.com/data/',
+    url,
+    headers: {
+      authorization: `Apikey ${API_KEY}`
+    },
     qs: {
+      fsym,
+      tsym,
       limit,
-      period,
     },
   }, (err, res, body) => {
-    callback(err, JSON.parse(body).slice(0, -1).map(e => ({
-      time: e.timestamp,
+    callback(err, JSON.parse(body).Data.slice(0, -1).map(e => ({
+      time: moment.unix(e.time).toDate(),
       open: +e.open,
-      high: +e.max,
-      low: +e.min,
+      high: +e.high,
+      low: +e.low,
       close: +e.close,
     })));
   });
