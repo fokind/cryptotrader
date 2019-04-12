899c9d0c317b2ef24f17dfa18e9f5b738f004b06
Dmitry Fokin <fokind1@yandex.ru>
2019-04-12 22:54:12 +0300

    тикер как сущность с доступом по ключу

--- server/src/ts/models/Ticker.ts
+++ server/src/ts/models/Ticker.ts
@@ -1,6 +1,14 @@
 import { Edm } from "odata-v4-server";
 
 export class Ticker {
+  @Edm.Key
+  @Edm.String
+  public currency: string;
+
+  @Edm.Key
+  @Edm.String
+  public asset: string;
+
   @Edm.Double
   public ask: number;
 
