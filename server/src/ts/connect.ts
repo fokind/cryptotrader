a99815fb4fecaf957c9b1c0cc43bf0c909ac25f8
Dmitry Fokin <fokind1@yandex.ru>
2019-04-25 22:27:23 +0300

    Оптимизация подключения к базе данных

--- server/src/ts/connect.ts
+++ server/src/ts/connect.ts
@@ -1,9 +1,14 @@
 import { MongoClient, Db } from "mongodb";
 
+let connect: MongoClient;
+
 export default async function(): Promise<Db>{
     const uri = process.env.NODE_ENV === "production" ?
     "mongodb://localhost:27017/crypto" :
     "mongodb://localhost:27017/crypto";
+
+    if (!connect) connect = await MongoClient.connect(uri);
+    if (!connect.isConnected()) await connect.connect();
     
-    return (await MongoClient.connect(uri)).db('crypto');
+    return connect.db('crypto');
 };
