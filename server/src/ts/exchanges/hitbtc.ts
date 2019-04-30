909611a5fa1edbfb37079c38403668a22e4ff4fa
Dmitry Fokin <fokind1@yandex.ru>
2019-04-30 19:16:20 +0300

    Feature/exchanges (#39)
    * биржи отделены от движка
    
    * Добавлена статистика из биржи hitbtc

---
+++ server/src/ts/exchanges/hitbtc.ts
@@ -0,0 +1,268 @@
+import * as request from 'request';
+import * as async from 'async';
+import { Order } from '../models/Order';
+import { SideEnum, IExchange, IMarketDataSource } from '../engine/Exchange';
+import moment = require('moment');
+
+const BASE_URL = 'https://api.hitbtc.com/api/2/';
+
+export class Hitbtc implements IExchange, IMarketDataSource {
+  async createOrder(options: {
+    currency: string,
+    asset: string,
+    side: SideEnum,
+    quantity: number,
+    price: number,
+    user: string,
+    pass: string
+  }): Promise<void> {
+    const { user, pass, asset, currency, side, quantity, price } = options;
+    return new Promise<void>((resolve, reject) => {
+      request.post({
+        baseUrl: BASE_URL,
+        url: 'order',
+        auth: {
+          user,
+          pass
+        },
+        json: true,
+        body: {
+          symbol:  asset + currency,
+          side: side ? 'buy' : 'sell',
+          quantity,
+          price,
+        }
+      }, (err) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  };
+
+  async getOrders(options: {
+    currency: string,
+    asset: string,
+    user: string,
+    pass: string
+  }): Promise<Array<Order>> {
+    const { currency, asset, user, pass } = options;
+    return new Promise<Array<Order>>((resolve, reject) => {
+      const TIMEOUT = 100;
+      let orders;
+      // TODO добавить счетчик, ограничивающий число попыток
+      async.doDuring(
+        callback => request.get({
+          baseUrl: BASE_URL,
+          url: 'order',
+          qs: {
+            symbol: asset + currency
+          },
+          auth: {
+            user,
+            pass
+          }
+        }, (err, res, body) => callback(undefined, res ? res.statusCode : undefined, body)),
+        (statusCode, body, callback) => {
+          if (statusCode === 200) {
+            orders = JSON.parse(body).map(e => new Order({
+              _id: e.clientOrderId,
+              // createdAt: e.createdAt,
+              // currency,
+              // asset,
+              // side: e.side,
+              // quantity: +e.quantity,
+              price: +(e.type === 'stopMarket' ? e.stopPrice : e.price),
+            }));
+
+            callback(undefined, false);
+          } else setTimeout(() => { callback(undefined, true); }, TIMEOUT);
+        },
+        err => !err ? resolve(orders) : reject(err)
+      );
+    });
+  };
+
+  async cancelOrders(options: {
+    currency: string,
+    asset: string,
+    user: string,
+    pass: string
+  }): Promise<void> {
+    const { currency, asset, user, pass } = options;
+    return new Promise<void>((resolve, reject) => {
+      request.delete({
+        baseUrl: BASE_URL,
+        url: 'order',
+        auth: {
+          user,
+          pass
+        },
+        body: {
+          symbol: asset + currency
+        },
+        json: true,
+      }, (err) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  };
+
+  async getSymbol(options: {
+    currency: string,
+    asset: string,
+  }): Promise<{ quantityIncrement: number, takeLiquidityRate: number }> {
+    const { currency, asset } = options;
+    return new Promise<{ quantityIncrement: number, takeLiquidityRate: number }>((resolve, reject) => {
+      const TIMEOUT = 100;
+      let symbol;
+      // TODO добавить счетчик, ограничивающий число попыток
+      async.doDuring(
+        callback => request.get({
+          baseUrl: BASE_URL,
+          url: 'public/symbol/' + asset + currency
+        }, (err, res, body) => callback(undefined, res ? res.statusCode : undefined, body)),
+        (statusCode, body, callback) => {
+          if (statusCode === 200) {
+            const { quantityIncrement, takeLiquidityRate } = JSON.parse(body);
+            symbol = {
+              quantityIncrement: +quantityIncrement,
+              takeLiquidityRate: +takeLiquidityRate
+            };
+            callback(undefined, false);
+          } else setTimeout(() => { callback(undefined, true); }, TIMEOUT);
+        },
+        err => !err ? resolve(symbol) : reject(err)
+      );
+    });
+  };
+
+  async getTicker(options: {
+    currency: string,
+    asset: string,
+  }): Promise<{ ask: number, bid: number }> {
+    const { currency, asset } = options;
+    return new Promise<{ ask: number, bid: number }>((resolve, reject) => {
+      const TIMEOUT = 100;
+      let ticker;
+      // TODO добавить счетчик, ограничивающий число попыток
+      async.doDuring(
+        callback => request.get({
+          baseUrl: BASE_URL,
+          url: 'public/ticker/' + asset + currency
+        }, (err, res, body) => callback(undefined, res ? res.statusCode : undefined, body)),
+        (statusCode, body, callback) => {
+          if (statusCode === 200) {
+            const { ask, bid } = JSON.parse(body);
+            ticker = {
+              ask: +ask,
+              bid: +bid
+            };
+            callback(undefined, false);
+          } else setTimeout(() => { callback(undefined, true); }, TIMEOUT);
+        },
+        err => !err ? resolve(ticker) : reject(err)
+      );
+    });
+  };
+  
+  async getPortfolio(options: {
+    user: string,
+    pass: string,
+  }): Promise<Array<{ currency: string, available: number }>> {
+    const { user, pass } = options;
+    return new Promise<Array<{ currency: string, available: number }>>((resolve, reject) => {
+      const TIMEOUT = 100;
+      let portfolio;
+      // TODO добавить счетчик, ограничивающий число попыток
+      async.doDuring(
+        callback => request.get({
+          baseUrl: BASE_URL,
+          url: 'trading/balance',
+          auth: {
+            user,
+            pass,
+          }
+        }, (err, res, body) => callback(undefined, res ? res.statusCode : undefined, body)),
+        (statusCode, body, callback) => {
+          if (statusCode === 200) {
+            portfolio = JSON.parse(body).filter(e => e.available !== "0").map(e => ({
+              currency: e.currency,
+              available: +e.available
+            }));
+            callback(undefined, false);
+          } else setTimeout(() => { callback(undefined, true); }, TIMEOUT);
+        },
+        err => !err ? resolve(portfolio) : reject(err)
+      );
+    });
+  };
+
+  async getCandles(options: {
+    currency: string,
+    asset: string,
+    period: string,
+    begin?: Date, // FIXME заменить на число или строку, чтобы исключить часовой пояс для даты
+    end?: Date
+  }): Promise<Array<{
+    time: Date,
+    open: number,
+    high: number,
+    low: number,
+    close: number
+  }>> {
+    const { currency, asset, period, begin, end } = options;
+    const url = `public/candles/${asset}${currency}`;
+    const qs: any = {
+      period
+    };
+
+    if (begin) qs.from = moment(begin).toISOString();
+
+    let candles = [];
+    const MAX_LIMIT = 1000;
+    const TIMEOUT = 100;
+    let from = moment(begin);
+    let duration = begin ? moment(end).diff(begin, period === 'M1' ? 'm' : (period === 'H1' ? 'h' : 'd')) : MAX_LIMIT;
+
+    while (duration > 0) {
+      qs.limit = Math.min(duration, MAX_LIMIT);
+      if (qs.limit) candles = candles.concat(await new Promise<any>(resolve => {
+        const options = {
+          baseUrl: BASE_URL,
+          url,
+          qs,
+        };
+        request.get(options, (err, res, body) => {
+          if (err) console.log(err);
+          if (res && res.statusCode === 200) {
+            resolve(JSON.parse(body).slice(0, -1).map(e => (<any>{
+              time: moment(e.timestamp).toDate(),
+              open: +e.open,
+              high: +e.max,
+              low: +e.min,
+              close: +e.close,
+            })));
+            duration -= qs.limit;
+          } else {
+            setTimeout(() => { resolve([]); }, TIMEOUT);
+          }
+        });
+      }));
+      from = from.add(qs.limit, period === 'M1' ? 'm' : (period === 'H1' ? 'h' : 'd'));
+      qs.from = from.toISOString();
+    }
+
+    return candles;
+  };
+};
--- server/src/ts/models/MarketData.ts
+++ server/src/ts/models/MarketData.ts
@@ -2,7 +2,7 @@ import { ObjectID } from "mongodb";
 import { Edm, odata } from "odata-v4-server";
 import { Candle } from "./Candle";
 import * as _ from 'lodash';
-import { MarketDataEngine } from "../engine/Market";
+import { ExchangeEngine } from "../engine/Exchange";
 import connect from "../connect";
 import * as moment from 'moment';
 
@@ -42,45 +42,25 @@ export class MarketData {
 
   @Edm.Action
   async update(@odata.result result: any, @Edm.DateTimeOffset begin?: Date, @Edm.DateTimeOffset end?: Date): Promise<number> {
-    // если указано начало, то найти наименьшее из текущего начала и параметра
-    // если указан конец, то найти наибольшее из текущего и параметра
     const db = await connect();
     const marketData = await db.collection("marketData").findOne({ _id: this._id });
     const { currency, asset, period, _id, begin: marketDataBegin, end: marketDataEnd } = marketData;
+    const exchange = 'hitbtc';
 
-    // первый более свежие, если дата конца позднее текущей
-    // берем разницу между параметром и текущей последней датой
-
-    // если текущих нет
-    // если периоды не указаны, и текущих нет, тогда без параметров, и всё
-
-    // если текущие есть, тогда только до конца
-    // если текущие есть и конец до текущего, тогда не делаем
-
-    // если указано начало, а текущие начало после, тогда с начала до начала
-    // если указано начало, а текущие начало до, тогда ничего не делать
-    
     let diff: Candle[];
 
     if (!marketDataEnd) {
-      diff = (await MarketDataEngine
-        .getCandles({ currency, asset, period, begin, end })).map(e => new Candle(e));
+      diff = (await ExchangeEngine
+        .getCandles(exchange, { currency, asset, period, begin, end })).map(e => new Candle(e));
     } else {
-      diff = (moment(end).isAfter(marketDataEnd) ? (await MarketDataEngine
-        .getCandles({ currency, asset, period, begin: moment(marketDataEnd).add(1, 'm').toDate(), end })) : []).concat(
-          moment(begin).isBefore(marketDataBegin) ? (await MarketDataEngine
-            .getCandles({ currency, asset, period, begin, end: moment(marketDataBegin).add(-1, 'm').toDate() })) : []
+      diff = (moment(end).isAfter(marketDataEnd) ? (await ExchangeEngine
+        .getCandles(exchange, { currency, asset, period, begin: moment(marketDataEnd).add(1, 'm').toDate(), end })) : []).concat(
+          moment(begin).isBefore(marketDataBegin) ? (await ExchangeEngine
+            .getCandles(exchange, { currency, asset, period, begin, end: moment(marketDataBegin).add(-1, 'm').toDate() })) : []
           ).map(e => new Candle(e));
     }
     
-    // разбить на два раза, если начало и конец выходит за рамки
-    // 
-    // если текущих нет, то просто обновить
-    // если текущие есть, то есть и то и другое
-    // если новое начало меньше текущего
-    // const diff = marketData.end ? candles.filter(e => moment(e.time).isAfter(marketData.end)) : candles;
     const { length } = diff;
-    // console.log(3, length);
 
     if (length) {
       let deltaBegin = marketDataBegin ? marketDataBegin : diff[0].time;
@@ -108,49 +88,7 @@ export class MarketData {
     }
   }
 
-  // @Edm.Action
-  // async import(@odata.result result: any, @Edm.DateTimeOffset begin?: Date, @Edm.DateTimeOffset end?: Date): Promise<number> {
-  //   const db = await connect();
-  //   const marketData = await db.collection("marketData").findOne({ _id: this._id });
-  //   const { currency, asset, period, _id, begin: currentBegin, end: currentEnd } = marketData;
-  //   // TODO запросить только то, что нужно
-  //   // дополнить более ранними записями
-  //   // дополнить более поздними записями
-  //   if (moment(begin).isBefore(currentBegin)) {
-      
-  //   }
-  //   const candles = (await MarketDataEngine
-  //     .getCandles({ currency, asset, period, begin, end })).map(e => new Candle(e));
-    
-  //   const diff = marketData.end ? candles.filter(e => moment(e.time).isAfter(marketData.end)) : candles;
-  //   const { length } = diff;
-  //   if (length) {
-  //     const delta: any = {
-  //       end: candles[candles.length - 1].time
-  //     };
-  //     if (!begin) {
-  //       delta.begin = candles[0].time;
-  //     }
-  //     const db = await connect();
-  //     await db.collection("candle").insertMany(diff.map(e => {
-  //       e.marketDataId = _id;
-  //       return e;
-  //     }));
-  //     await db.collection("marketData").updateOne({ _id }, { $set: delta });
-  //     return 1;
-  //   } else {
-  //     return 0;
-  //   }
-  // }
-
   constructor(jsonData: any) {
     Object.assign(this, jsonData);
   }
 }
-
-/*
-сделать так, чтобы статистика накапливалась в системе, а для этого необходимо добавить рынки
-эксперты подключаются к рынкам, следят за появлением новых свечей
-у эксперта нельзя просто так поменять символ или период, для этого его нужно связать с другим рынком
-точно так же нужно связать с другой стратегией, или поменять параметры стратегии
-*/
